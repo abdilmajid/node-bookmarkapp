2f7ce7bd80909fb6163c000297d098e6f6df94f5
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2018-11-15 09:38:27 -0500

    connected server and database

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+
+// require('dotenv').config();
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : 'localhost',
+    user : 'postgres',
+    password : '123456',
+    database : 'booklist'
+  }
+});
 
 
+db.select('*').from('books').then(data => {
+  console.log(data);
+})
 
 
 const app = express();
@@ -14,21 +31,39 @@ mustache.cache = null;
 app.engine('mustache', mustache);
 app.set('view engine', 'mustache');
 
+
 app.use(express.static('public'));
+// Extended true ->is a nested  
+// Extended False ->not nested object
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
 
 //Where we put the booklist
 app.get('/list', (req, res) => {
   res.render('list');
 }) 
 
-
-
-
-
-
-
-
-
-app.listen(process.env.PORT, () => {
-  console.log(`listening on server ${process.env.PORT}`)
+app.get('/book/add', (req, res) => {
+  res.render('bookForm')
 })
+
+app.post('/book/add', (req, res) => {
+  const { title, author } = req.body;
+  db('books').insert({
+    title: title,
+    author: author
+  })
+  .then(() => {
+    res.redirect('/list');
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+ });
+
+
+
+app.listen(process.env.PORT || 5006, () => {
+  console.log(`App Running on port ${process.env.PORT}`)
+});
