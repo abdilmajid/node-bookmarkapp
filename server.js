abcddb30f15dee0cbbc62c1a6ca807a5d01839ee
Abdilmajid Ibrahim <abdilmajidibrahim@gmail.com>
2018-11-15 22:19:11 -0500

    Got the connection with Database and front end to work

--- server.js
+++ server.js
@@ -2,30 +2,23 @@ const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const knex = require('knex');
-
-// require('dotenv').config();
-
-const db = knex({
-  client: 'pg',
-  connection: {
-    host : 'localhost',
-    user : 'postgres',
-    password : '123456',
-    database : 'booklist'
-  }
-});
+const pg = require('pg');
 
 
-db.select('*').from('books').then(data => {
-  console.log(data);
-})
-
+const path = require('path');
 
 const app = express();
+// Database string
+const config = {
+  user: 'postgres',
+  database: 'booklist',
+  password: 123456,
+  port: 5432,
+}
+
+const pool = new pg.Pool(config);
+
 const mustache = mustacheExpress();
-
-
 
 mustache.cache = null; 
 app.engine('mustache', mustache);
@@ -39,30 +32,42 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
+
 //Where we put the booklist
-app.get('/list', (req, res) => {
-  res.render('list');
+app.get('/books', (req, res) => {
+  pool.connect((err, client, done) => {
+    if(err) {
+      console.log('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM books', (err, result) => {
+      done();
+      if(err) {
+        console.log('error running query', err);
+      }
+      res.render('book-list', {books: result.rows});
+    })
+  })
 }) 
 
 app.get('/book/add', (req, res) => {
-  res.render('bookForm')
+  res.render('book-form')
 })
 
+
 app.post('/book/add', (req, res) => {
-  const { title, author } = req.body;
-  db('books').insert({
-    title: title,
-    author: author
-  })
-  .then(() => {
-    res.redirect('/list');
-  })
-  .catch((err) => {
-    console.log(err);
-  })
+  console.log('works');
+  pool.connect()
+    .then(() => {
+      const sql = 'INSERT INTO books (title, author) VALUES ($1, $2)'
+      const params = [req.body.title, req.body.author];
+      return pool.query(sql, params);
+    })
+    .then((result) => {
+      console.log('result?', result);
+      res.redirect('/books');
+    })
+
  });
-
-
 
 app.listen(process.env.PORT || 5006, () => {
   console.log(`App Running on port ${process.env.PORT}`)
